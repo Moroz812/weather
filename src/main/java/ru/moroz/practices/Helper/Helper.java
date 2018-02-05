75bf36cc48f03311cfe1181857ec10efb34e867c
Moroz812 <antigenspb@gmail.com>
2018-02-05 22:27:21 +0300

    создал класс Helper в котором описано http соединение, которое вдальнейшем надо заменить на retrofit, тем самым "качнуться"

---
+++ src/main/java/ru/moroz/practices/Helper/Helper.java
@@ -0,0 +1,49 @@
+package ru.moroz.practices.Helper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by moroz on 05.02.18.
+ */
+
+public class Helper {
+    static  String stream = null;
+
+    public Helper() {
+
+    }
+
+    public  String getHTTPDate (String urlString) {
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+            if (httpURLConnection.getResponseCode() == 200) //OK = 200
+            {
+                BufferedReader r = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = r.readLine()) != null)
+                    sb.append(line);
+                stream = sb.toString();
+                httpURLConnection.disconnect();
+            }
+
+
+
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return  stream;
+
+    }
+}
---
+++ src/main/java/ru/moroz/practices/Model/Model.java
@@ -0,0 +1,8 @@
+package ru.moroz.practices.Model;
+
+/**
+ * Created by moroz on 05.02.18.
+ */
+
+public class Model {
+}
