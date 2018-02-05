b9f9db8202186638a14642939f7ac578cd74310c
Moroz812 <antigenspb@gmail.com>
2018-02-05 17:46:16 +0300

    создание общего класса Common, в котором указан API_KEY, регулярное выражение, формат даты и тд.

---
+++ src/main/java/ru/moroz/practices/Common/Common.java
@@ -0,0 +1,40 @@
+package ru.moroz.practices.Common;
+
+import android.support.annotation.NonNull;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by moroz on 05.02.18.
+ */
+
+public class Common {
+    public static String API_KEY = "340daad7e220c7f6f5d76389177e3111";
+    public static String API_LINK = "api.openweathermap.org/data/2.5/weather";
+
+    @NonNull
+    public static String apiRequest (String lat, String lng) {
+        StringBuilder sb = new StringBuilder(API_LINK);
+        sb.append(String.format("?lat=%s&lon=%s&APPID=%s&units=metric",lat,lng,API_KEY));
+        return sb.toString();
+    }
+
+    public static String unixTimeStampToDateTime (double unixTimeStamp) {
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+        Date date = new Date();
+        date.setTime((long) unixTimeStamp*1000);
+        return dateFormat.format(date);
+    }
+
+    public static String getImage (String icon) {
+        return String.format("http://openweathermap.org/img/w/%s.png", icon);
+    }
+
+    public static String getDateNow () {
+        DateFormat dateFormat = new SimpleDateFormat("dd MMM yyyy HH:mm");
+        Date date = new Date ();
+        return dateFormat.format(date);
+    }
+}
