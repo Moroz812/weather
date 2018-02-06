9596153d0e0dcd100f2bdfbcafc8fdfadbb45806
Moroz812 <antigenspb@gmail.com>
2018-02-06 08:32:01 +0300

    созданы классы для объяктов json

---
+++ src/main/java/ru/moroz/practices/Model/Clouds.java
@@ -0,0 +1,21 @@
+package ru.moroz.practices.Model;
+
+/**
+ * Created by moroz on 05.02.18.
+ */
+
+public class Clouds {
+    private int all;
+
+    public Clouds(int all) {
+        this.all = all;
+    }
+
+    public int getAll() {
+        return all;
+    }
+
+    public void setAll(int all) {
+        this.all = all;
+    }
+}
--- src/main/java/ru/moroz/practices/Model/Model.java
+++
@@ -1,8 +0,0 @@
-package ru.moroz.practices.Model;
-
-/**
- * Created by moroz on 05.02.18.
- */
-
-public class Model {
-}
